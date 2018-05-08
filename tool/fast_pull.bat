7af17e63cf35e8c82a4143bab89ae072fe9539fa
jiniannet <hnvvv@163.com>
2018-05-08 10:55:41 +0800

    PULL BAT

--- tool/fast_pull.bat
+++ tool/fast_pull.bat
@@ -1,1 +1,1 @@
-git push --progress "https://github.com/jiniannet/jnt.py.git" refs/heads/master:refs/heads/master
+git push --progress "https://gitee.com/jiniannet/jnt.py.git" refs/heads/master:refs/heads/master
